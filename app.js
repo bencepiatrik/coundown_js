716378a7cb78d8bfc54c058a884abc79d703c82a
bencepiatrik <bence.piatrik@azet.sk>
2026-03-20 17:06:57 +0100

    first commit

--- app.js
+++ app.js
@@ -1,47 +1,6 @@
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-const giveaway = document.querySelector(".giveaway");
-const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
-const nextYearContainer = document.querySelector('.next-year') 
-const lastYearContainer = document.querySelector('.last-year') 
 
-let nextYear = (new Date().getFullYear()) + 1
-
-nextYearContainer.innerHTML = nextYear
-lastYearContainer.innerHTML = nextYear -1
-
-let futureDate = new Date(nextYear, 0, 1, 0, 0, 0);
-const year = futureDate.getFullYear();
-const hours = futureDate.getHours();
-const minutes = futureDate.getMinutes();
-
-let month = futureDate.getMonth();
-month = months[month];
-const date = futureDate.getDate();
-const weekday = weekdays[futureDate.getDay()];
+const futureDate = new Date(2026, 3, 17, 23, 59, 59); // April 17, 2026 23:59:59
 const futureTime = futureDate.getTime();
 
 function getRemainingTime() {
@@ -52,19 +11,16 @@ function getRemainingTime() {
   const oneHour = 60 * 60 * 1000;
   const oneMinute = 60 * 1000;
 
-  let days = t / oneDay;
-  days = Math.floor(days);
-
+  let days = Math.floor(t / oneDay);
   let hours = Math.floor((t % oneDay) / oneHour);
   let minutes = Math.floor((t % oneHour) / oneMinute);
   let seconds = Math.floor((t % oneMinute) / 1000);
+
   const values = [days, hours, minutes, seconds];
+
   function format(item) {
-    if (item < 10 && item > 0) {
-      return (item = `0${item}`);
-    }
-     else if(item <= 0) {
-      return (item = item)
+    if (item < 10 && item >= 0) {
+      return `0${item}`;
     }
     return item;
   }
